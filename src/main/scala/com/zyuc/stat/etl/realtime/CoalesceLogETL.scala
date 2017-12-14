6259fccdf06a940cafb3598a79d75f138c14f058
hadoop <1090136337@qq.com>
2017-12-14 21:36:15 +0800

    1. CoalesceLogETL--

--- src/main/scala/com/zyuc/stat/etl/realtime/CoalesceLogETL.scala
+++ src/main/scala/com/zyuc/stat/etl/realtime/CoalesceLogETL.scala
@@ -10,17 +10,17 @@ import org.apache.spark.sql.hive.HiveContext
 import scala.collection.mutable
 
 /*
-  * Created by hadoop on 17-12-13.
-  * @author x
+  * Created by hadoop on 17-12-14.
+  * @author liuzk
   */
 object CoalesceLogETL {
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf().setMaster("local[3]").setAppName("name_201712101132")
+    val sparkConf = new SparkConf().setMaster("local[3]").setAppName("name_201712101130")
     val sc = new SparkContext(sparkConf)
     val sqlContext = new HiveContext(sc)
 
 
-    val appName = sc.getConf.get("spark.app.name")//,"name_201712101135")
+    val appName = sc.getConf.get("spark.app.name")//,"name_201712101130")
     val inputPath = sc.getConf.get("spark.app.inputPath", "hdfs://spark1234:8020/hadoop/IOT/data/cdr/output/pgw/")
     val coalesceSize = sc.getConf.get("spark.app.coalesce.size", "200").toInt
 
@@ -32,11 +32,11 @@ object CoalesceLogETL {
     val partitionPath = s"/d=$d/h=$h/m5=$m5"
 
     // 将data/d=$d/h=$h/m5=$m5/下文件移动到 mergeTmp/${dataTime}/ 目录下
-    FileUtils.move2Temp(fileSystem, inputPath, partitionPath)
+    FileUtils.move2Temp(fileSystem, inputPath, dataTime)
 
-    val tmpPath = new Path(inputPath + "mergeTmp/" +  dataTime )
+    val tmpPath = (new Path(inputPath + "mergeTmp/" +  dataTime )).toString
     val coalesce = FileUtils.makeCoalesce(fileSystem, tmpPath, coalesceSize)
-    val srcDF = sqlContext.read.format("orc").load(coalPath2).coalesce(coalesce)
+    val srcDF = sqlContext.read.format("orc").load(tmpPath).coalesce(coalesce)
     srcDF.write.mode(SaveMode.Overwrite).format("orc").save(inputPath + "data" + partitionPath+"/")
 
     val tmpFiles = new Path(inputPath + "mergeTmp/" +  dataTime +"/*.orc")
